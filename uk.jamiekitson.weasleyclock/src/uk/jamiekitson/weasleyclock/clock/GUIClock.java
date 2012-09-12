ccf4979a4d408928a8a47ba3d4b8a8c6b443176d
Jamie Kitson <Jamie@JamieKitson.co.uk>
2012-09-12 21:30:44 +0100

    Test

--- uk.jamiekitson.weasleyclock/src/uk/jamiekitson/weasleyclock/clock/GUIClock.java
+++ uk.jamiekitson.weasleyclock/src/uk/jamiekitson/weasleyclock/clock/GUIClock.java
@@ -6,25 +6,23 @@ import org.eclipse.paho.client.mqttv3.MqttTopic;
 
 public class GUIClock implements Clock
 {
+	
 	@Override
 	public void connectionLost(Throwable arg0)
 	{
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deliveryComplete(MqttDeliveryToken arg0)
 	{
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void messageArrived(MqttTopic arg0, MqttMessage arg1) throws Exception
 	{
-		// TODO Auto-generated method stub
-		
+		// Handle new messages
 	}
 
 }
